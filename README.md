7068614e687cf2b6b71e39b31a9baa02a7167f46
Qiyu Li <0x9357@gmail.com>
2016-11-20 14:29:51 +0800

    add to dic method and fix bugs

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ b=true
 
 //index.js
 const conf2p = require('conf2p')
-console.log(conf2p('input.conf')) // --a=3 --b=true
+let obj = new conf2p('input.conf')
+console.log(await obj.toString()) // --a=3 --b=true
+console.log(await obj.toDic()) // {a: 3, b: true}
 ```
--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ import test from 'ava'
 import conf2p from '../index'
 
 test('test',async t => {
-    t.is(await conf2p(__dirname + '/testInput.conf'),
+    var obj = new conf2p(__dirname + '/testInput.conf')
+    t.is(await obj.toString(),
         ' --a=3 --b=true --lite-x=disable --dir=/123/456')
 })
